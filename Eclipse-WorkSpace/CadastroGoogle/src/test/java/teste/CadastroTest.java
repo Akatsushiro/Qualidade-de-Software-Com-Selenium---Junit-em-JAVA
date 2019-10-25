bf7ff7a2d0f650e60ab0efffb01bbd88ec8cbb8a
akatsushiro <patrickdantas999@gmail.com>
2019-10-24 22:34:34 -0300

    Teste de cadastro no site seubarriga.wcaquino.me

--- Eclipse-WorkSpace/CadastroGoogle/src/test/java/teste/CadastroTest.java
+++ Eclipse-WorkSpace/CadastroGoogle/src/test/java/teste/CadastroTest.java
@@ -14,33 +14,6 @@ import pages.Cadastro;
 
 public class CadastroTest {
 
-	static WebDriver driver;
-	static Cadastro cadastro;
-	@BeforeClass
-	public static void setUpBeforeClass() throws Exception {
-		System.setProperty("webdriver.chrome.driver", "./resources/chromedriver.exe");
-		driver = new ChromeDriver();
-		driver.get("https://accounts.google.com/signup/v2/webcreateaccount?flowName=GlifWebSignIn&flowEntry=SignUp");
-		cadastro = new Cadastro(driver);
-	}
-
-	@AfterClass
-	public static void tearDownAfterClass() throws Exception {
-	}
-
-	@Before
-	public void setUp() throws Exception {
-	}
-
-	@After
-	public void tearDown() throws Exception {
-		//driver.close()
-	}
-
-	@Test
-	public void test() {
-		cadastro.preencherCampos();
-		assertEquals(cadastro.validarMensagem(), "Escolha um endereço do Gmail");
-	}
+	
 
 }
